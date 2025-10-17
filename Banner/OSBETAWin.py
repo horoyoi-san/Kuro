cc809ae2376f14a663300439b685ec9a100bc3c9
Horoyoi-san <185492733+horoyoi-san@users.noreply.github.com>
2025-10-17 21:46:30 +0700

    test

--- Banner/OSBETAWin.py
+++ Banner/OSBETAWin.py
@@ -82,6 +82,7 @@ def send_webhooks(data, url, title):
     for webhook_url in webhook_urls:
         send_webhook(data, url, title, webhook_url)
 
+# ================= Discord =================
 def send_webhook(data, url, title, webhook_url):
     if not webhook_url:
         print(f"⚠️ Webhook URL ไม่ถูกต้อง, ข้ามการส่ง")
@@ -94,15 +95,17 @@ def send_webhook(data, url, title, webhook_url):
 
     resource = default_data.get("resource")
     if resource:
+        # Launcher
         version = resource.get("version", "No version")
         path = resource.get("path", "")
         md5 = resource.get("md5", "")
         size = resource.get("size", 0)
         cdn_list = default_data.get("cdnList", [])
         full_url = cdn_list[0]["url"] + path if cdn_list and path else path
-        #patch_text = "Launcher resource"
+        patch_embeds = []  # Launcher ไม่มี patch
         cdn_text = "\n".join([cdn["url"] for cdn in cdn_list]) if cdn_list else "None"
     else:
+        # Game
         config = default_data.get("config", {})
         version = config.get("version", "No version")
         size = config.get("size", 0)
@@ -116,9 +119,15 @@ def send_webhook(data, url, title, webhook_url):
             index_file = patch.get("indexFile")
             full_url_patch = cdn_list[0]["url"] + index_file if cdn_list else index_file
             patch_versions.append(f"{ver}: {full_url_patch}")
-        patch_text = "\n".join(patch_versions) if patch_versions else "None"
+        patch_text = "\n".join(patch_versions) if patch_versions else None
+
+        if patch_text:
+            patch_embeds = create_patch_embeds(patch_text)
+        else:
+            patch_embeds = []
 
         full_url = patch_versions[-1].split(": ")[-1] if patch_versions else "No URL"
+
 
     extra_url = "https://static1.anpoimages.com/wordpress/wp-content/uploads/2024/05/wuthering-waves-hero-resized-16-9.jpg"
 
