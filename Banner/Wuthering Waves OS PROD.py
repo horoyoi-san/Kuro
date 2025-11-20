31380d562270cbc2fb2ecbd103571ef65f189388
Horoyoi-san <185492733+horoyoi-san@users.noreply.github.com>
2025-11-20 15:11:15 +0700

    Update Wuthering Waves OS PROD.py

--- Banner/Wuthering Waves OS PROD.py
+++ Banner/Wuthering Waves OS PROD.py
@@ -7,8 +7,8 @@ from datetime import datetime, timezone
 # ================= Webhook =================
 webhook_urls = [
     os.environ.get("WEBHOOK1"),
-  #  os.environ.get("WEBHOOK2"),
- #   os.environ.get("WEBHOOK3"),
+    os.environ.get("WEBHOOK2"),
+    os.environ.get("WEBHOOK3"),
 ]
 
 # ================= Logging =================
