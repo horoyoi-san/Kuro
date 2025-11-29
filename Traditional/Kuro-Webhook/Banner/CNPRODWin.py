1ec9c2bed2ebc3b08ceb797aa29d56f7b25e8c6e
Horoyoi-san <185492733+horoyoi-san@users.noreply.github.com>
2025-11-30 03:36:17 +0700

    Delete url webhook

--- Traditional/Kuro-Webhook/Banner/CNPRODWin.py
+++ Traditional/Kuro-Webhook/Banner/CNPRODWin.py
@@ -4,9 +4,9 @@ import json
 
 # กำหนด Webhook URLs
 webhook_urls = {
-    "Wuthering Waves": 'https://discord.com/api/webhooks/1313874393532989532/0mN1RuiIcN9zDC4HmE4PIOcAPN7B73tgX2TUHMpQH3EkmRTiy5LizlR1PZsnf-J0RSQs',
-    "Wuthering Waves 2": 'https://discord.com/api/webhooks/1315273814132785273/KCEkUloeo75HpgwrEVhXDfRzLSuOB7LHf0Nm1zCme0I1s-bl_jkujpcVZC8KSKifEkNU',
-    "Teat": 'https://discord.com/api/webhooks/1291725154937999444/CeBZotZNDREE7KM7mFx7DJ--Z2TD8tKKmfgZ8gqPUrLs2Bs2rALXjm6HPqv_VKNxGfQJ'
+    "Wuthering Waves": ' ',
+    "Wuthering Waves 2": ' ',
+     "Teat": ' '
 }
 
 # ตัวแปรสำหรับเก็บข้อมูลล่าสุด
