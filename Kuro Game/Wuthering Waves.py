692f3c78a7e840f0356e032ad14d000c654940fc
Horoyoi-san <korrawitcompara@gmail.com>
2026-06-26 19:59:04 +0700

    -jpg +png

--- Kuro Game/Wuthering Waves.py
+++ Kuro Game/Wuthering Waves.py
@@ -27,7 +27,7 @@ BOT_NAME = "Wuthering Waves"
 
 BOT_ICON = (
     "https://raw.githubusercontent.com/"
-    "horoyoi-san/Kuro/refs/heads/Webhook/assets/ww.jpg"
+    "horoyoi-san/Kuro/refs/heads/Webhook/assets/ ww2.png"
 )
 
 # =========================================================
